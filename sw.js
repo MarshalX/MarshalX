d892c98b77c25a0906dd92961da5a66cbcf381a8
MarshalX <MarshalX@users.noreply.github.com>
2024-12-28 16:14:45 +0000

    Deploying to gh-pages from @ MarshalX/MarshalX@87de54db60edc98e3e07428b0a043b4b3a739251 🚀

--- sw.js
+++ sw.js
@@ -36,17 +36,17 @@ self.__precacheManifest = [
     "url": "styles-66ab959efbc95922d7cb.js"
   },
   {
-    "url": "commons-5c2d245b1d4f607b7f50.js"
+    "url": "commons-6c586315571f8ab617db.js"
   },
   {
-    "url": "app-60013283db65e1d70ab1.js"
+    "url": "app-6de3e5b220c110059165.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-ceef87afc058962c4db9.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "5f9255c98927aef13802362fa4caa863"
+    "revision": "b01d803c7d2bdf76ec03c864e22d793b"
   },
   {
     "url": "manifest.webmanifest",
@@ -143,7 +143,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-60013283db65e1d70ab1.js`))) {
+  if (!resources || !(await caches.match(`/app-6de3e5b220c110059165.js`))) {
     return await fetch(event.request)
   }
 
